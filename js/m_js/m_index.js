6b4e84a2bd9aeb5b7083f0362f453575ecdd2ea6
imtangry <tang.rui,yu@foxmial.com>
2018-06-11 18:40:49 +0800

    add

--- js/m_js/m_index.js
+++ js/m_js/m_index.js
@@ -1,3 +1,10 @@
-var Carousel=function (ele) {
-    
-}
+var Carousel = function (ele) {
+    // 暂时默认轮播图数量大于三个
+    var ul = ele.document.getElementsByTagName('ul'),
+        imgs_container = ul[0],
+        pointer_container = ul[1],
+        imgs_li = imgs_container.document.getElementsByTagName('li'),
+        pointer_li = pointer_container.document.getElementsByTagName('li'),
+        imgs_length = imgs_li.length;
+
+        }
